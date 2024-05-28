1ef5591e03c1f2e66a543722b6284616259b112d
Your Name <mfaridzdzuhreza124@gmail.com>
2024-05-28 14:31:46 +0700

    portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#020617',
+        secondary: '#64748b',
+        ig: '#cd486b',
+        linkedin: '#007bb5',
+        wa: '#25d366'
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
+
