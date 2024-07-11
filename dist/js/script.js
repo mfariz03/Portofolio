fefdb042dbe386a2d23df031467af948eb9c2f0c
Your Name <mfaridzdzuhreza124@gmail.com>
2024-07-11 10:50:52 +0700

    hapus lorem di sertifikat

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,10 +12,37 @@ hamburger.addEventListener('click', function() {
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixednav = header.offsetTop;
+    const totop = document.querySelector('#to-Top');
 
     if(window.pageYOffset > fixednav) {
         header.classList.add('navbar-fixed');
+        totop.classList.remove('hidden');
+        totop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        totop.classList.remove('flex');
+        totop.classList.add('hidden');
     }
 }
+
+//klik close hamburger
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navmenu) {
+        hamburger.classList.remove('hamburger-active');
+        navmenu.classList.add('hidden')
+    }
+})
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if  (darkToggle.checked) {
+        html.classList.add('dark')
+    } else {
+        html.classList.remove('dark')
+    }
+})
+
+
